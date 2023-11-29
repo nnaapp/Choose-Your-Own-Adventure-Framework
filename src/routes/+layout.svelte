209489a234a70d7275a66c152b15171da9db057a
nnaapp <connor@jnj.org>
2023-11-29 11:41:38 -0600

    Added page transitions

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
     import '../global.css';
     import { gameTree } from '../tree_store.ts';
+    import { onNavigate } from '$app/navigation';
 
     let tree: StoryTree;
     gameTree.subscribe((value) => {
@@ -11,6 +12,53 @@
     let primaryFont = tree.fonts[0];
     let backupFont = tree.fonts[1];
     let genericFont = tree.fonts[2];
+
+    // From SvelteKit guide on page transitions
+    onNavigate((navigation) => {
+        if (!document.startViewTransition) return;
+
+	      return new Promise((resolve) => {
+		        document.startViewTransition(async () => {
+                resolve();
+                await navigation.complete;
+            });
+        });
+    });
 </script>
+
+<style>
+@keyframes fade-in {
+	from {
+		opacity: 0;
+	}
+}
+
+@keyframes fade-out {
+	to {
+		opacity: 0;
+	}
+}
+
+@keyframes slide-from-right {
+	from {
+		transform: translateX(30px);
+	}
+}
+
+@keyframes slide-to-left {
+	to {
+		transform: translateX(-30px);
+	}
+}
+
+:root::view-transition-old(root) {
+	animation: 90ms cubic-bezier(0.4, 0, 1, 1) both fade-out, 300ms cubic-bezier(0.4, 0, 0.2, 1) both slide-to-left;
+}
+
+:root::view-transition-new(root) {
+	animation: 210ms cubic-bezier(0, 0, 0.2, 1) 90ms both fade-in, 300ms cubic-bezier(0.4, 0, 0.2, 1) both
+			slide-from-right;
+}
+</style>
 
 <slot />
